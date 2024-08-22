1e85433f7a8efd7f17266e47fe41316af677c439
Afqan <efocan444@gmail.com>
2024-08-22 13:13:07 +0400

    photo size fixed and random located add

--- turtle odevi/turtle yakala.py
+++ turtle odevi/turtle yakala.py
@@ -1,14 +1,28 @@
+from PIL import Image
 import turtle
-image=(r"C:\Users\firdo\Desktop\phyton projects\images - Copy\output-onlinegiftools.gif")
-turtle.register_shape(image)
+from random import randint
 
+# Görüntüyü yükle ve yeniden boyutlandır
+image_path = r"C:\Users\firdo\Desktop\phyton projects\images - Copy\output-onlinegiftools.gif"
+image = Image.open(image_path)
+resized_image = image.resize((80, 80))  # Boyutları değiştir, örneğin 100x100 piksel
+resized_image_path = r"C:\Users\firdo\Desktop\phyton projects\images - Copy\resized_output.gif"
+resized_image.save(resized_image_path)
 
-win=turtle.Screen()
-win.bgcolor("blue")
-win.title("oyun")
-player=turtle.Turtle()
-player.shape(image)
-player.forward(100)
+# Turtle'da yeniden boyutlandırılmış görüntüyü kullan
+turtle.register_shape(resized_image_path)
+def window():
+    win = turtle.Screen()
+    win.bgcolor("blue")
+    win.title("oyun")
+def plyr():
+    player = turtle.Turtle()
+    player.shape(resized_image_path)
+    player.pensize(9)
+    player.penup()
+    #random yere goturme
+    player.goto(randint(-100,0),randint(0,100))
 
-
+window()
+plyr()
 turtle.mainloop()
